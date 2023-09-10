8c2e00c8d3afbbc875a58e76e4ea41d523db9fcf
Emirlan <abdykadyrov.emirlan.00@gmail.com>
2023-09-11 02:38:40 +0600

    tcreate end

---
+++ src/components/create.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import "./Create.css"; // Импортируем стили
+import { HiOutlineChevronDown, HiOutlineChevronUp } from "react-icons/hi";
+
+const Create = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const incrementAge = () => {
+    setAge(age + 1);
+  };
+
+  const decrementAge = () => {
+    if (age > 0) {
+      setAge(age - 1);
+    }
+  };
+
+  const handleAddItem = () => {
+    // Ваш код для добавления элемента в базу данных
+    console.log("Name:", name);
+    console.log("Age:", age);
+    // Сбросить значения полей после добавления
+    setName("");
+    setAge(0);
+  };
+
+  return (
+    <div className="main">
+      {/* Имя */}
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="name-input"
+      />
+
+      {/* Возраст */}
+      <div className="custom-input">
+        <input
+          type="number"
+          value={age}
+          placeholder="Age"
+          onChange={(e) => setAge(Number(e.target.value))}
+          className="age-input"
+        />
+        <button className="decrement-button" onClick={decrementAge}>
+          <HiOutlineChevronDown />
+        </button>
+        <button className="increment-button" onClick={incrementAge}>
+          <HiOutlineChevronUp />
+        </button>
+      </div>
+
+      {/* Выбор темы */}
+      <div className="subject">
+        <select
+          placeholder="Subject line"
+          name="subject"
+          id="subject_input"
+          required
+          className="subject-select"
+        >
+          <option className="subbb" disabled hidden selected>
+            Subject line
+          </option>
+          <option>I'd likeject</option>
+          <option>I'd like to ask a question</option>
+          <option>I'd like to make a proposal</option>
+        </select>
+        <button className="subject-decrement-button">
+          <HiOutlineChevronDown />
+        </button>
+      </div>
+
+      <div className="toggle-container">
+        <input type="checkbox" id="switch" className="toggle-input" />
+      </div>
+
+      <div className="button-container">
+        <button className="insert-button">Insert</button>
+        <label htmlFor="switch" className="toggle-label">
+          Mode
+        </label>
+        <button className="delete-button">Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Create;
